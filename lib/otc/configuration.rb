0bce9468dc1d94bc6271ed23207dc688407a4cfa
Christian Paling <christian.paling@googlemail.com>
2017-09-06 16:26:57 +0200

    Add bang methods for every configuration

--- lib/otc/configuration.rb
+++ lib/otc/configuration.rb
@@ -1,10 +1,38 @@
 module Otc
   class Configuration
+    class Missing < RuntimeError; end
+
     class << self
       attr_accessor :project, :username, :password, :region, :domainname
 
       def configure
         yield self
+      end
+
+      def reset
+        @project = nil
+        @username = nil
+        @password = nil
+        @region = nil
+        @domainname = nil
+      end
+
+      def method_missing(m, *args, &block)
+        m_as_string = m.to_s
+
+        if m_as_string.end_with?("!")
+          method = m_as_string.split("!").first.to_sym
+
+          if self.respond_to?(method)
+            send(method).tap do |result|
+              raise Missing, "missing configuration #{method}" if result.nil?
+            end
+          else
+            super
+          end
+        else
+          super
+        end
       end
     end
   end
--- spec/otc/configuration_spec.rb
+++ spec/otc/configuration_spec.rb
@@ -1,6 +1,8 @@
 require "spec_helper"
 
 RSpec.describe Otc::Configuration do
+  before(:each) { Otc::Configuration.reset }
+
   describe "::configure" do
     it "should require a block" do
       expect { Otc::Configuration.configure }.to raise_error(LocalJumpError)
@@ -18,12 +20,34 @@ RSpec.describe Otc::Configuration do
       Otc::Configuration.project = "cool_project"
       expect(Otc::Configuration.project).to eq("cool_project")
     end
+
+    context "!" do
+      it "should raise an error if project is nil" do
+        expect { Otc::Configuration.project! }.to raise_error(Otc::Configuration::Missing)
+      end
+
+      it "should return the project if it is not nil" do
+        Otc::Configuration.project = "cool_project"
+        expect(Otc::Configuration.project).to eq("cool_project")
+      end
+    end
   end
 
   describe "::username" do
     it "should have it as an attribute accessor" do
       Otc::Configuration.username = "testuser"
       expect(Otc::Configuration.username).to eq("testuser")
+    end
+
+    context "!" do
+      it "should raise an error if username is nil" do
+        expect { Otc::Configuration.username! }.to raise_error(Otc::Configuration::Missing)
+      end
+
+      it "should return the username if it is not nil" do
+        Otc::Configuration.username = "testuser"
+        expect(Otc::Configuration.username).to eq("testuser")
+      end
     end
   end
   
@@ -32,6 +56,17 @@ RSpec.describe Otc::Configuration do
       Otc::Configuration.password = "test1234"
       expect(Otc::Configuration.password).to eq("test1234")
     end
+
+    context "!" do
+      it "should raise an error if password is nil" do
+        expect { Otc::Configuration.password! }.to raise_error(Otc::Configuration::Missing)
+      end
+
+      it "should return the password if it is not nil" do
+        Otc::Configuration.password = "test1234"
+        expect(Otc::Configuration.password).to eq("test1234")
+      end
+    end
   end
 
   describe "::region" do
@@ -39,12 +74,34 @@ RSpec.describe Otc::Configuration do
       Otc::Configuration.region = "eu-de"
       expect(Otc::Configuration.region).to eq("eu-de")
     end
+
+    context "!" do
+      it "should raise an error if region is nil" do
+        expect { Otc::Configuration.region! }.to raise_error(Otc::Configuration::Missing)
+      end
+
+      it "should return the region if it is not nil" do
+        Otc::Configuration.region = "eu-de"
+        expect(Otc::Configuration.region).to eq("eu-de")
+      end
+    end
   end
 
   describe "::domainname" do
     it "should have it as an attribute accessor" do
       Otc::Configuration.domainname = "OTC-0001"
       expect(Otc::Configuration.domainname).to eq("OTC-0001")
+    end
+
+    context "!" do
+      it "should raise an error if domainname is nil" do
+        expect { Otc::Configuration.domainname! }.to raise_error(Otc::Configuration::Missing)
+      end
+
+      it "should return the domainname if it is not nil" do
+        Otc::Configuration.domainname = "OTC-0001"
+        expect(Otc::Configuration.domainname).to eq("OTC-0001")
+      end
     end
   end
 end
