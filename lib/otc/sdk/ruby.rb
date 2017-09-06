9dac34a6d8360b6f5950b47d59f00e5e64be1a68
Christian Paling <christian.paling@googlemail.com>
2017-09-06 14:27:21 +0200

    Initial commit

---
+++ lib/otc/sdk/ruby.rb
@@ -0,0 +1,9 @@
+require "otc/sdk/ruby/version"
+
+module Otc
+  module Sdk
+    module Ruby
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/otc/sdk/ruby/version.rb
@@ -0,0 +1,7 @@
+module Otc
+  module Sdk
+    module Ruby
+      VERSION = "0.1.0"
+    end
+  end
+end
