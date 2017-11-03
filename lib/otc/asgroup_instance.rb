b41bbf26a5715ad43c0a80d30f7614b78705ecb7
Christian Paling <christian.paling@googlemail.com>
2017-11-03 10:14:12 +0100

    Add new property to ASGroup Instance due to API changes

--- lib/otc/asgroup_instance.rb
+++ lib/otc/asgroup_instance.rb
@@ -4,7 +4,8 @@ module Otc
   class ASGroupInstance < Hashie::Dash
     [
       "instance_id", "scaling_group_id", "scaling_group_name", "life_cycle_state", "health_status",
-      "scaling_configuration_name", "scaling_configuration_id", "create_time", "instance_name"
+      "scaling_configuration_name", "scaling_configuration_id", "create_time", "instance_name",
+      "protect_from_scaling_down"
     ].each { |prop| property prop }
   end
 end
