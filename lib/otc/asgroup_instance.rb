6e3de60573e09edbf706e6309a0275618bd1c7b8
Christian Paling <christian.paling@googlemail.com>
2018-07-19 09:27:44 +0200

    Change from strict Hashie::Dash to more flexible Hashie::Mash. Also add more Ruby versions for Travis

--- lib/otc/asgroup_instance.rb
+++ lib/otc/asgroup_instance.rb
@@ -1,11 +1,6 @@
 require "hashie"
 
 module Otc
-  class ASGroupInstance < Hashie::Dash
-    [
-      "instance_id", "scaling_group_id", "scaling_group_name", "life_cycle_state", "health_status",
-      "scaling_configuration_name", "scaling_configuration_id", "create_time", "instance_name",
-      "protect_from_scaling_down"
-    ].each { |prop| property prop }
+  class ASGroupInstance < Hashie::Mash
   end
 end
--- lib/otc/ecs.rb
+++ lib/otc/ecs.rb
@@ -1,14 +1,11 @@
 require "hashie"
 
 module Otc
-  class ECS < Hashie::Dash
+  class ECS < Hashie::Mash
     class << self
       def query_all(name: nil)
         response = Request.get service: "ecs", path: "/v2/#{Configuration.project!}/servers/detail?name=#{name}"
         JSON.parse(response.body)["servers"].map do |server|
-          keys_to_delete = server.keys - ECS::ATTRIBUTES
-          keys_to_delete.each { |key| server.delete(key) }
-
           ECS.new(server)
         end
       end
@@ -17,13 +14,6 @@ module Otc
         query_all(name: name).first
       end
     end
-
-    ATTRIBUTES = [
-      "name", "id", "status", "created", "updated", "flavor", "image", "tenant_id", "key_name", 
-      "user_id", "metadata", "hostId", "addresses", "security_groups", "tags", "links", "progress"
-    ]
-
-    ATTRIBUTES.each { |prop| property prop }
 
     def public_ip
       @_public_ip ||= begin
